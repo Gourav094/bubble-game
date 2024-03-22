7337074fbf35571e7c56ffe42d2364c0ad5f3ccc
gourav garg <garggourav012@gmail.com>
2024-03-22 21:37:34 +0530

    fix: moving arrows

--- index.js
+++ index.js
@@ -5,22 +5,35 @@ var context = canvas.getContext('2d')
 // define circle configurations like colour,radius,border
 var circleRadius = 30
 var circleBorderWidth = 1;
-var circleColors = ['yellow', 'powderblue', 'red', 'lightgreen']
+var circleColors = ['#F6D471', '#1D8ECE', '#CE1D1D', '#49BD4E']
 
 // cicle initial position
 var circleX = 70;
 var initialY = 50;
 var circleSpacing = 70;
 
+// define arrows configuration
 var arrowWidth = 10
 var arrowSpeed = 5
-var isArrowMoving = false;
 
 // Arrays to store circle and arrow positions
 var circlePositions = [];
 var arrowPositions = [];
+var arrayMoving = [false,false,false,false]
 
-var clickedCircle = -1
+// initialise arrow points 
+function initialisation(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < circleColors.length; i++) {
+        var circleY = initialY + (circleRadius * 2 + circleSpacing) * i;
+        var arrowStartX = canvas.width - 50;
+        var arrowStartY = circleY;
+        var arrowEndX = canvas.width - circleRadius - 100
+
+        circlePositions.push({ x: circleX, y: circleY });
+        arrowPositions.push({ x: arrowStartX, y: arrowStartY ,endX:arrowEndX });
+    }
+}
 
 function drawCircles(){
     for (let i = 0; i < circleColors.length; i++) {
@@ -37,38 +50,20 @@ function drawCircles(){
     }
 }
 
-function initialisation(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < circleColors.length; i++) {
-        var circleY = initialY + (circleRadius * 2 + circleSpacing) * i;
-        var arrowStartX = canvas.width - 50;
-        var arrowStartY = circleY;
-        var arrowEndX = canvas.width - circleRadius - 100
-
-        circlePositions.push({ x: circleX, y: circleY });
-        arrowPositions.push({ x: arrowStartX, y: arrowStartY ,endX:arrowEndX });
-    }
-}
-initialisation()
-
 function drawArrows(){
     for (let i = 0; i < arrowPositions.length; i++) {
         drawArrow(arrowPositions[i].x, arrowPositions[i].y, arrowPositions[i].endX, arrowPositions[i].y);
     }
 }
 
-
 function drawArrow(fromx, fromy, tox, toy){
-
     var width = arrowWidth
     var angle = Math.atan2(toy - fromy, tox - fromx);
 
     var headlen = 6;
-    // This makes it so the end of the arrow head is located at tox, toy, don't ask where 1.15 comes from
     tox -= Math.cos(angle) * ((width * 1.15));
     toy -= Math.sin(angle) * ((width * 1.15));
     
-    //starting path of the arrow from the start square to the end square and drawing the stroke
     context.beginPath();
     context.moveTo(fromx, fromy);
     context.lineTo(tox, toy);
@@ -76,24 +71,19 @@ function drawArrow(fromx, fromy, tox, toy){
     context.lineWidth = width;
     context.stroke();
     
-    //starting a new path from the head of the arrow to one of the sides of the point
     context.beginPath();
     context.moveTo(tox, toy);
     context.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
     
-    //path from the side point of the arrow, to the other side point
     context.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),toy-headlen*Math.sin(angle+Math.PI/7));
     
-    //path from the side point back to the tip of the arrow, and then again to the opposite side point
     context.lineTo(tox, toy);
     context.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
 
     //draws the paths created above
     context.fillStyle = "black";
-    // context.strokeStyle = "#cc0000";
     context.lineWidth = width;
     context.stroke();
-    // context.fillStyle = "#cc0000";
     context.fill();
 }
 
@@ -103,9 +93,8 @@ function draw(){
     drawArrows();
 }
 
-
 function animateArrow(clickedCircleIndex) {
-    isArrowMoving = true;
+    arrayMoving[clickedCircleIndex] = true
     var arrowIndex = clickedCircleIndex;
     var targetCircleX = circleX;
     var targetCircleY = initialY + (circleRadius * 2 + circleSpacing) * clickedCircleIndex;
@@ -122,9 +111,10 @@ function animateArrow(clickedCircleIndex) {
         // since arrow length is 100 so stop the moving arrow after touching circle boundary
         if (distance < 100) {
             clearInterval(interval);
-            isArrowMoving = false;
-            // Change circle color immediately
-            circleColors[clickedCircleIndex] = getRandomColor(); // Change to desired color
+            arrayMoving[clickedCircleIndex] = false
+
+            // Change circle color
+            circleColors[clickedCircleIndex] = getRandomColor(); 
         }
         console.log(distance)
         // Update arrow position
@@ -138,21 +128,27 @@ function animateArrow(clickedCircleIndex) {
     }, 20);
 }
 
-
 function reset() {
-    if(!isArrowMoving){
-        circleColors = ['yellow', 'powderblue', 'red', 'lightgreen'];
-        arrowPositions.forEach(function (arrow, index) {
-            arrow.x = canvas.width - 50;
-            arrow.endX = canvas.width - circleRadius - 100;
-        });
-        isArrowMoving = false;
-        draw();
+    const isMoving = arrayMoving.some(function (isMoving) {
+        return isMoving;
+    }) 
+
+    if(isMoving){
+        return
     }
+
+    // Reset colour and positions
+    circleColors = ['#F6D471', '#1D8ECE', '#CE1D1D', '#49BD4E']
+    arrowPositions.forEach(function (arrow, index) {
+        arrow.x = canvas.width - 50;
+        arrow.endX = canvas.width - circleRadius - 100;
+    });
+
+    arrayMoving = [false,false,false,false];
+    draw();
 }
 
 canvas.addEventListener('click', function (event) {
-    if (!isArrowMoving) {
         var rect = canvas.getBoundingClientRect();
         var mouseX = event.clientX - rect.left;
         var mouseY = event.clientY - rect.top;
@@ -160,25 +156,14 @@ canvas.addEventListener('click', function (event) {
         for (var i = 0; i < circlePositions.length; i++) {
             var circle = circlePositions[i];
             if (Math.sqrt((mouseX - circle.x) ** 2 + (mouseY - circle.y) ** 2) <= circleRadius) {
-                // if (clickedCircle !== i) {
-                    animateArrow(i);
-                    // clickedCircle = i;
-                // }
+                    if(!arrayMoving[i]){
+                        animateArrow(i);
+                        arrayMoving[i] = true
+                    }
                 break;
             }
         }
-    }
 });
-
-document.getElementById('reset-button').addEventListener('click', function () {
-    console.log("clicked reset button")
-    reset();
-});
-
-
-draw()
-
-
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -188,3 +173,12 @@ function getRandomColor() {
     }
     return color;
 }
+
+document.getElementById('reset-button').addEventListener('click', function () {
+    console.log("clicked reset button")
+    reset();
+});
+
+initialisation()
+
+draw()
